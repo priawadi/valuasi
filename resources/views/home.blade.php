c2665daedd05fef4631091960e48cb4d528ee799
Ozi Priawadi <jie.ilkom46ipb@gmail.com>
2016-07-17 21:51:41 +0700

    Add Login Module

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title')
+    Kuesioner
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    Home
+                    <br>
+<!--                     <a href="{{ url('responden') }}" class="btn btn-primary">
+                        <i class="fa fa-btn fa-sign-in"></i>Lanjut 
+                    </a>
+-->					
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,23 +45,26 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/responden') }}">
-                    Kuesioner Valuasi
+                    Kuesioner
                 </a>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/responden') }}">Responden</a></li>
-                    <li><a href="{{ url('/export') }}">Export</a></li>
+					@if (Auth::guest())
+					<!-- <li><a href="{{ url('/') }}">Home</a></li> -->
+					@else
+                    <!-- <li><a href="{{ url('/responden') }}">Responden</a></li> -->
+ 					<li><a href="{{ url('/export') }}">Export</a></li>
+					@endif
                 </ul>
 
                 <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
+                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::guest())
-<!--                         <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li> -->
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+						<!-- <li><a href="{{ url('/register') }}">Register</a></li> -->
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
